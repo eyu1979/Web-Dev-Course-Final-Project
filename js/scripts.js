e66a2d6e7b29f21611dc5e7edfa6ea13c13e5f2b
eyu1979 <137987941+eyu1979@users.noreply.github.com>
2025-03-10 11:39:56 -0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function(){
+   $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn img').toggleClass("active");
+    });
+
+});
